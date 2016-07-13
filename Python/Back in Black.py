274eab06b24f83cbdb11581673e6aa2143ffdc2c
Navid Mamoon <navidmx@gmail.com>
2016-07-13 14:38:08 -0400

    More changes

--- Python/Back in Black.py
+++ Python/Back in Black.py
@@ -1,33 +1,31 @@
 #Back in Black
 from Myro import *
-init("sim")
 i=0
 x=0
+
 def riff1():
     forward(2,.4)
     wait(.5)
     x=0
     while x<2:
-        forward(2,.1)
-        wait(.1)
-        forward(2,.1)
-        wait(.1)
-        forward(2,.1)
-        wait(.5)
+        forward(2,.05)
+        wait(.001)
+        forward(2,.05)
+        wait(.6)
         x=x+1
-    motors(3,-3,1.62)
+    motors(3,-3,1.4)
     
 def riff2():
     forward(2,.4)
     wait(.5)
     x=0
     while x<2:
-        forward(2,.1)
-        wait(.1)
-        forward(2,.1)
-        wait(.1)
-        forward(2,.1)
-        wait(.5)
+        forward(2,.05)
+        wait(.001)
+        forward(2,.05)
+        wait(.001)
+        forward(2,.05)
+        wait(.2)
         x=x+1
     x=0
     while x<4:
@@ -35,12 +33,16 @@ def riff2():
         forward(1,.06)
         x=x+1
     
+beep(1,999)
+wait(4.01)
+    
 while i<4: #Verse
+    riff1()
+    riff2()
     i=i+1
-    while i<6:
-        forward(2, 1.9)
-        motors(-10, 10, .5)
-        i=i+1
-       
+#    while i<6:
+#        forward(2, 1.9)
+#        motors(-10, 10, .5)
+#        i=i+1
     
 stop()
---
+++ Python/Workshop.py
@@ -0,0 +1,5 @@
+def addfive(number):
+    number = number + 5
+    return number
+
+print(addfive(5))
