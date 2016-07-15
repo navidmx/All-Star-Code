91c23ef9a2cd27294eebd65f051633a40eedd7c9
Navid Mamoon <navidmx@gmail.com>
2016-07-15 10:52:19 -0400

    List Lab

---
+++ Python/List Lab.py
@@ -0,0 +1,40 @@
+#Problem 1
+List1=[10,11,12,13,14,15,16,17,18,19,20]
+
+#Problem 2
+List2=[0,0,0,0,0]
+
+#Problem 3
+List3=[]
+
+#Problem 4
+i=0
+List4=[]
+while i<5:
+    List4.append(0)
+    i=i+1
+
+#Problem 5
+List5=["a", 5, "doodle", 3, 10]
+print(len(List5))
+
+#Problem 6
+del List5[3]
+
+#Problem 7
+List5.append(17)
+
+#Problem 8
+List5[0]=8.4
+
+#Problem 9
+List5.insert(0,"I broke in here")
+print(List5)
+
+#Problem 10
+List10=[]
+i=1
+while i<10:
+    List10.append(i)
+    i=i+2
+print(List10)
--- Python/Workshop.py
+++ Python/Workshop.py
@@ -1,1 +1,8 @@
-from Myro import *
+x=5
+myList=["whatevs", x, 2.2]
+print(myList[2])
+myList[2]="Navid"
+print(myList[2])
+myList.append("Mamoon")
+del myList[2]
+print(len(myList))
